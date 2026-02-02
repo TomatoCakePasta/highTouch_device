277a36958e2f5cb4c006513a70e20ec6915bb0a4
TomatoCakePasta <gaomuk50@gmail.com>
2026-02-02 14:59:06 +0900

    Seeeduinoのコードを追加

--- xiao_touch/xiao_touch.ino
+++ xiao_touch/xiao_touch.ino
@@ -1,12 +1,14 @@
-#include <DFPlayer_Mini_Mp3.h>
+#include <DFPlayerMini_Fast.h>
+#include <SoftwareSerial.h>
 
-#include "SoftwareSerial.h"
+#define PIN_MP3_RX 7
+#define PIN_MP3_TX 6
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial ss_mp3_player(PIN_MP3_RX, PIN_MP3_TX);
+DFPlayerMini_Fast mp3_player;
 
 const int TOTAL_TRACKS = 3; // total songs in the SD card
 
-// Threshold
 const int THRESHOLD = 600;
 
 struct PlayerState {
@@ -16,23 +18,33 @@ struct PlayerState {
 PlayerState player = {1};
 
 void setup() {
-  // pressure sensor
-  pinMode(A0, INPUT);
-
-  mySerial.begin(9600);
-  mp3_set_serial(mySerial);
-  mp3_set_volume(50);
-  delay(1000);
-  mp3_play(1);
-  delay(1000);
-  mp3_play(2);
-  delay(1000);
-  mp3_play(3);
-  delay(1200);
-
-  mp3_stop(); // 初期状態は停止
-
   Serial.begin(9600);
+  delay(1000);
+
+  Serial.println("Start");
+
+  ss_mp3_player.begin(9600);
+  delay(1000);
+
+  if (!mp3_player.begin(ss_mp3_player)) {
+    Serial.println("DFPlayer not found");
+    while (1);
+  }
+
+  mp3_player.reset();
+  delay(1500);
+
+  Serial.println("DFPlayer ready");
+
+  mp3_player.volume(30);
+  delay(1500);
+
+  mp3_player.play(1);
+  delay(1000);
+  mp3_player.play(1);
+  delay(1000);
+  mp3_player.play(2);
+  delay(1000);
 }
 
 void loop() {
@@ -43,9 +55,8 @@ void loop() {
   if (isTouched) {
     playSound();
   }
+}
 
-  delay(10);
-} 
 
 bool checkTouch() {
   static int cnt = 0;
@@ -79,7 +90,7 @@ void playSound() {
   }
 
   Serial.println("Play");
-  mp3_play(player.currentTrack);
+  mp3_player.play(player.currentTrack);
   delay(600);
 
   player.currentTrack++;
